b739f38dd5b94b4cba144c18aa1aae4789227ffa
apa3a <apa3a>
2007-11-04 16:11:47 +0000

    Improved javadoc generation.

--- com.mousefeed/build.sh
+++ com.mousefeed/build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # runs build
 
-CP="lib/ant-1.7.0.jar:lib/ant-launcher-1.7.0.jar:lib/ant-junit-1.7.0.jar"
-java -cp $CP org.apache.tools.ant.launch.Launcher -f build1.xml $@ 
+CP="$JAVA_HOME/lib/tools.jar:lib/ant-1.7.0.jar:lib/ant-launcher-1.7.0.jar:lib/ant-junit-1.7.0.jar"
+$JAVA_HOME/bin/java -cp $CP org.apache.tools.ant.launch.Launcher -f build1.xml $@ 
      
