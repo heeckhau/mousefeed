d5a732ba69e5460f1ed9dde0246f908150f1be46
Hendrik Eeckhaut <hendrik.eeckhaut@sigasi.com>
2011-01-04 16:15:44 +0100

    removed old build files

--- com.mousefeed/build.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-# runs build
-
-CP="$JAVA_HOME/lib/tools.jar:lib/ant-1.7.0.jar:lib/ant-launcher-1.7.0.jar:lib/ant-junit-1.7.0.jar"
-$JAVA_HOME/bin/java -cp $CP org.apache.tools.ant.launch.Launcher -f build1.xml $@ 
-     
